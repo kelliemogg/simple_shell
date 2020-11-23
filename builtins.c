c73364b2375b4767e87e18ea29e5e9a6479be556
Kellie Mogg <kelliedmogg@gmail.com>
2020-11-23 18:06:08 +0000

    betty fixes

--- builtins.c
+++ builtins.c
@@ -1,13 +1,22 @@
 #include "header.h"
 
-/* Edited to be able to compile - problem: unused variables */
+/**
+ * sh_exit - this exits the func
+ * description: exit builtin
+ * Return: 0
+ */
 
-int sh_exit()
+int sh_exit(void)
 {
 	return (0);
 }
 
-int sh_env()
+/**
+ * sh_env - prints the environ
+ * description: env builtin
+ * Return: 0
+ */
+int sh_env(void)
 {
 	int i;
 	int j;
@@ -26,6 +35,12 @@ int sh_env()
 	return (1);
 }
 
+/**
+ * sh_cd - changes directory
+ * description: this is change dir
+ * @argv: arg passed
+ * Return: 0
+ */
 int sh_cd(char **argv)
 {
 	if (argv[1] == NULL)
@@ -37,15 +52,24 @@ int sh_cd(char **argv)
 	}
 	return (1);
 }
-int sh_help()
+
+/**
+ * sh_help - help builtin
+ * description: this is the help
+ * Return: 0
+ */
+int sh_help(void)
 {
 	char *available = "The following commands are available:\n";
 	char *cd = "cd\n";
 	char *exit = "exit\n";
 
-	/*write(STDOUT_FILENO, "This is Tyler McClure and Kellie Mogg's Simmple Shell\n");*/
-        write(STDOUT_FILENO, available, stringlength(available));
-        write(STDOUT_FILENO, cd, stringlength(cd));
-        write(STDOUT_FILENO, exit, stringlength(exit));
+	/**
+	 *write(STDOUT_FILENO, "This is Tyler McClure and Kellie Mogg's
+	 *Simple Shell\n");
+	 */
+	write(STDOUT_FILENO, available, stringlength(available));
+	write(STDOUT_FILENO, cd, stringlength(cd));
+	write(STDOUT_FILENO, exit, stringlength(exit));
 	return (1);
 }
--- header.h
+++ header.h
@@ -12,8 +12,12 @@
 #include <strings.h>
 #include <stddef.h>
 
-/* made struct for builtins*/
-
+/**
+ * struct builtins - struct with builtin commands
+ * @argv: arg passed as command
+ * @func: correlations function
+ * description: this is to handle the builtins
+ */
 typedef struct builtins
 {
 	char *argv;
@@ -26,19 +30,19 @@ char **tokenize(char *userinput);
 
 int stringlength(char *s);
 
-void shell_loop();
+void shell_loop(void);
 
 int executor(char **argv);
 
 int function_finder(char **argv, va_list args_list);
 
-int sh_exit();
+int sh_exit(void);
 
-int sh_help();
+int sh_help(void);
 
-int sh_cd (char **argv);
+int sh_cd(char **argv);
 
-int sh_env();
+int sh_env(void);
 
 /* new prototypes associated with test.c*/
 
@@ -56,11 +60,5 @@ char *_strdup(char *str);
 char *_env_parser(char *name);
 char *_get_env(char *env);
 extern char **environ;
-
-typedef struct commander
-{
-        char *commad;
-        void (*cmd_func)();
-} commander;
 
 #endif /* HEADER_H */
