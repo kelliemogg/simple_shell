0112f06df88a1a8a4f61b95f658779322bd5fb12
Kellie Mogg <kelliedmogg@gmail.com>
2020-11-23 18:47:24 +0000

    deleted files

---
+++ builtins.c
@@ -0,0 +1,76 @@
+#include "header.h"
+
+/**
+ * sh_exit - this exits the func
+ * description: exit builtin
+ * Return: 0
+ */
+
+int sh_exit(void)
+{
+	return (0);
+}
+
+/**
+ * sh_env - prints the environ
+ * description: env builtin
+ * Return: 0
+ */
+int sh_env(void)
+{
+	int i;
+	int j;
+	char *string;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		for (j = 0; environ[i][j] != '\0'; j++)
+		{
+			string = _strdup(&(environ[i][j]));
+			write(STDOUT_FILENO, string, stringlength(string));
+			break;
+		}
+		_putchar('\n');
+	}
+	return (1);
+}
+
+/**
+ * sh_cd - changes directory
+ * description: this is change dir
+ * @argv: arg passed
+ * Return: 0
+ */
+int sh_cd(char **argv)
+{
+	if (argv[1] == NULL)
+		perror("Argument expected\n");
+	else
+	{
+		if (chdir(argv[1]) != 0)
+			perror("Invalid path\n");
+	}
+	return (1);
+}
+/**
+ * sh_cd - changes directory
+ * description: this is change dir
+ * @argv: arg passed
+ * Return: 0
+ */
+
+int sh_help(void)
+{
+	char *available = "The following commands are available:\n";
+	char *cd = "cd\n";
+	char *exit = "exit\n";
+
+	/**
+	 *write(STDOUT_FILENO, "This is Tyler McClure and Kellie Mogg's
+	 *Simple Shell\n");
+	 */
+	write(STDOUT_FILENO, available, stringlength(available));
+	write(STDOUT_FILENO, cd, stringlength(cd));
+	write(STDOUT_FILENO, exit, stringlength(exit));
+	return (1);
+}
---
+++ getenv.c
@@ -0,0 +1,59 @@
+#include "header.h"
+
+char *_get_env(char *env)
+{
+        int inner;
+        int outer;
+        char *name;
+
+        for (outer = 0; environ[outer] != NULL; outer++)
+        {
+                for (inner = 0; environ[outer][inner] != '='; inner++)
+                {
+                        /* printf("%c", environ[outer][inner]); */
+                        if (environ[outer][inner] != env[inner])
+                                break;
+                        if (environ[outer][inner] == env[inner])
+                        {
+                                if (env[inner + 1] == '\0' && environ[outer][inner + 1] == '=')
+                                {
+                                        name = _strdup(&(environ[outer][inner + 2]));
+                                        return(_env_parser(name));
+                                }
+                        }
+                }
+        }
+        return(NULL);
+}
+
+char *_env_parser(char *name)
+{
+        int token_inc;
+        int tokencount;
+        char *tokenize;
+	int i;
+        char **p;
+
+        tokencount = 0;
+        for(i = 0; name[i] != '\0'; i++)
+        {
+                if(name[i] == ':')
+                {
+                        tokencount++;
+                }
+        }
+        p = malloc(8 * (tokencount + 2));
+        if(p != NULL)
+        {
+                token_inc = 0;
+                tokenize = strtok(name, ":");
+                while(token_inc < (tokencount + 1))
+                {
+                        p[token_inc] = tokenize;
+                        tokenize = strtok(NULL, ":");
+                        printf("%s\n", p[token_inc]);
+                        token_inc++;
+                }
+        }
+        return (*p);
+}
